171c2ffe7f2538d40b5403449cce4d249396cf49
Donildo <donildobrito307@gmail.com>
2024-05-01 14:51:40 -0400

    comit

---
+++ TestesUnitario.Desafio.Console/Services/ValidacoesString.cs
@@ -0,0 +1,21 @@
+namespace TestesUnitario.Desafio.Console.Services
+{
+    public class ValidacoesString
+    {
+        public int RetornarQuantidadeCaracteres(string texto)
+        {
+            var numeroCaracteres = texto.Length;
+            return numeroCaracteres;
+        }
+        public bool ContemCaracteres(string texto, string textoProcurado)
+        {
+            var contem = texto.Contains(textoProcurado);
+            return contem;
+        }
+        public bool TextoTerminaCom(string texto, string textoProcurado)
+        {
+            var termina = texto.EndsWith(textoProcurado);
+            return termina;
+        }
+    }
+}
